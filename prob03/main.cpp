399b650d9544c17526dc0a6a94f34a8236566fc2
909jfguerrero <jfguerrero@csu.fullerton.edu>
2019-09-14 14:14:53 -0700

    did prob03

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,21 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  unsigned lastSales, thisSales;
+
+  //get the last years total Sales
+  std::cout << "What was the last year's sales?";
+  std::cin >> lastSales;
+
+  //calculate the years sales prediction
+  thisSales = (lastSales * .18) + lastSales;
+
+  //display this years sales prodiction
+  std::cout << "this years sales is " << thisSales << '\n';
+
+
+  return 0;
+}
